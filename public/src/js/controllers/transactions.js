331c69dabbfb86af686182eaac787865affcf2ea
rnicodemo <rnicodemo@deloitte.it>
2017-08-25 12:01:27 +0200

    var instead of let

--- public/src/js/controllers/transactions.js
+++ public/src/js/controllers/transactions.js
@@ -13,8 +13,8 @@ function($scope, $rootScope, $routeParams, $location, Global, Transaction, Trans
   function opReturnCleaning(hex) {
     hex = hex.split(" ");
     hex = hex[hex.lenght - 1];
-    let str = '';
-    for (let i = 0; i < hex.length; i += 2)
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
         str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
     return str;
   }
