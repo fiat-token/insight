ae5e6791ccc5124b812731ea02716512c6676266
rnicodemo <rnicodemo@deloitte.it>
2017-08-28 16:20:22 +0100

    module.exports singoli

--- public/src/js/lib/libServer.js
+++ public/src/js/lib/libServer.js
@@ -1,22 +1,16 @@
 var crypto = require('crypto');
 var jwt = require('jsonwebtoken');
 
-module.exports = { 
-        sendRes, 
-        validPassword,
-        generateJwt
-}
-
-function sendRes(res, status, content) {
+module.exports.sendRes = function (res, status, content) {
         res.status(status); 
         res.json(content);
 };
 
-function validPassword(hash, salt, password) {
+module.exports.validPassword = function (hash, salt, password) {
         return hash === crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha256').toString('hex'); 
 };
 
-function generateJwt (user, role) {
+module.exports.generateJwt = function (user, role) {
         var expiry = new Date();
         expiry.setDate(expiry.getDate() + 7);
         return jwt.sign({
