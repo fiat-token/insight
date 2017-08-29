9c6e869157f75999b6ef14800f58d771c96360b7
bgalante <bgalante@deloitte.it>
2017-08-29 17:12:34 +0200

    app-api and related deleted

--- public/app_api/models/user.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    hash: String,
-    salt: String
-});
-
-mongoose.model('User', userSchema);
