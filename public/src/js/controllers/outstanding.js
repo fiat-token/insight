26cbf52574d7e83a4e8740d1843046bd7af1f6f9
rnicodemo <rnicodemo@deloitte.it>
2017-11-24 10:40:07 +0000

    remove issuer balance from outstanding

--- public/src/js/controllers/outstanding.js
+++ public/src/js/controllers/outstanding.js
@@ -1,14 +1,29 @@
 'use strict';
 
 angular.module('insight.outstanding').controller('OutstandingController',
-    function ($scope, $http, Api) {
+    function ($scope, $rootScope, $http, Api) {
+        $scope.issuerAddress = "VQojEPjGcbiHr4UzEahegSdwwHeCqQKxCd";
         $scope.outstanding = {};
 
         $http.get(Api.apiPrefix + '/outstanding' )
             .success(function (txoutsetinfo, status, headers, config) {
                 $scope.outstanding = txoutsetinfo.result;
+                $scope.outstanding.total_amount = $rootScope.currency.getConvertionValue($scope.outstanding.total_amount);
+                
+                $http.get(Api.apiPrefix + '/addr/' + $scope.issuerAddress + '/balance')
+                .success(function (balance, status, headers, config) {
+                    if(balance)
+                        $scope.outstanding.total_amount -= (balance / $rootScope.currency.factor);
+    
+                })
+                .error(function (data, status, headers, config) {
+                    console.log("Error calling getaddress balance");
+                    console.log(data);
+                });
+
             })
             .error(function (data, status, headers, config) {
+                console.log("Error calling getoutstanding");
                 console.log(data);
             });
 
