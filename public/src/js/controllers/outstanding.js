49784b159c1a7d0453028dc6fa3824035bc3b00a
rnicodemo <rnicodemo@deloitte.it>
2017-11-28 22:17:20 +0000

    2 decimals

--- public/src/js/controllers/outstanding.js
+++ public/src/js/controllers/outstanding.js
@@ -8,12 +8,12 @@ angular.module('insight.outstanding').controller('OutstandingController',
         $http.get(Api.apiPrefix + '/outstanding' )
             .success(function (txoutsetinfo, status, headers, config) {
                 $scope.outstanding = txoutsetinfo.result;
-                $scope.outstanding.total_amount = ($scope.outstanding.total_amount * 10000).toFixed(4);
+                $scope.outstanding.total_amount = ($scope.outstanding.total_amount * 10000).toFixed(2);
                 
                 $http.get(Api.apiPrefix + '/addr/' + $scope.issuerAddress + '/balance')
                 .success(function (balance, status, headers, config) {
                     if(balance)
-                        $scope.outstanding.total_amount -= (balance / 10000).toFixed(4);
+                        $scope.outstanding.total_amount -= (balance / 10000).toFixed(2);
     
                 })
                 .error(function (data, status, headers, config) {
