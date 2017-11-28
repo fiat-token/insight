efad57d37e452d589d7539a1f20312ea116a4eee
rnicodemo <rnicodemo@deloitte.it>
2017-11-28 22:55:07 +0000

    managing satoshi in oustanding page

--- public/src/js/controllers/outstanding.js
+++ public/src/js/controllers/outstanding.js
@@ -8,13 +8,13 @@ angular.module('insight.outstanding').controller('OutstandingController',
         $http.get(Api.apiPrefix + '/outstanding' )
             .success(function (txoutsetinfo, status, headers, config) {
                 $scope.outstanding = txoutsetinfo.result;
-                $scope.outstanding.total_amount = ($scope.outstanding.total_amount * 10000).toFixed(2);
+                $scope.outstanding.total_amount = $scope.outstanding.total_amount * 1e8;
                 
                 $http.get(Api.apiPrefix + '/addr/' + $scope.issuerAddress + '/balance')
                 .success(function (balance, status, headers, config) {
                     if(balance)
-                        $scope.outstanding.total_amount -= (balance / 10000).toFixed(2);
-    
+                        $scope.outstanding.total_amount -= balance;
+
                 })
                 .error(function (data, status, headers, config) {
                     console.log("Error calling getaddress balance");
